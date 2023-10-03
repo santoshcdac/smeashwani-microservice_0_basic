9091f805186d876531c3e32f3c6575c7071fcf2f
Saxena, Ashwani <ashwani_saxena@optum.com>
2023-10-03 17:15:10 +0530

    Update

--- product/src/main/java/com/training/product/entity/ProductEntity.java
+++ product/src/main/java/com/training/product/entity/ProductEntity.java
@@ -1,12 +1,13 @@
 package com.training.product.entity;
 
 import jakarta.persistence.Entity;
-
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
+@Table(name = "product")
 @Entity
 @Data
 public class ProductEntity {
---
+++ product/src/main/resources/schema.sql
@@ -0,0 +1,1 @@
+create table product (id int not null AUTO_INCREMENT, name varchar(100) not null, description varchar(500), PRIMARY KEY ( id ));
